6d9431611f628655d7be68003d8c55c4d78287a1
iquach92 <iquach@icloud.com>
2018-07-29 13:45:55 +0300

    Revert "'a'"
    This reverts commit 2139d556fc8c4ec3398bb44eda36cb348e701b46.

--- js/main.js
+++ js/main.js
@@ -27,4 +27,22 @@
  //     images[i].className = 'showed';
  //
  // }
- $('.sl').slick();
+ $('.sl').slick({
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     infinite: true,
+     responsive: [
+         {
+             breakpoint: 1200,
+             settings: {
+                 slidesToShow: 2,
+             }
+         },
+         {
+             breakpoint: 600,
+             settings: {
+                 slidesToShow: 1,
+             }
+         },
+     ]
+ });
