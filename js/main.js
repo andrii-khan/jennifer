7f614f3bb72032e81ff18e68ad4d4cc951609419
iquach92 <iquach@icloud.com>
2018-07-29 12:47:03 +0300

    11

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-// var btn_left = document.querySelector('.partnerts-section .containr .scroll-left');
-// var btn_right = document.querySelector('.partnerts-section .containr .scroll-right');
-//
-// var images = document.querySelector('.partnerts-section .containr .partnerts-block .pbc img');
-// var i = 0;
-//
-//  btn_left.onclick = function () {
-//      images[i].className = '';
-//      i = i--;
-//
-//      if(i<0) {
-//          i = images.length - 1;
-//      }
-//
-//      images[i].className = 'showed';
-//
-//  }
-//
-// btn_right.onclick = function () {
-//     images[i].className = '';
-//     i = i++;
-//
-//     if(i>0) {
-//         i = 0;
-//     }
-//
-//     images[i].className = 'showed';
-//
-// }
 $('.sl').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
